c9e34805a7876dcc0c1c86cc059bc50cc5fa7008
P38LightningII <56324741+P38LightningII@users.noreply.github.com>
2024-02-14 11:55:36 -0500

    update

--- fragmentation_uncertainty/vimpel_case.py
+++ fragmentation_uncertainty/vimpel_case.py
@@ -14,13 +14,13 @@ import os
 
 from vimpel_functions import extract_vimpel, vimpel_to_state, vimpel_propagation
 from centaur_dict import Case09047B, Case14055B, Case18079B
-from orbit_conversions import xyz_to_rdx, ico_to_rdx
+from orbit_conversions import xyz_to_rdx, ico_to_rdx, eci_to_rdx
 from ode import two_body_ode, two_body_ode_with_perturbations
 import planetary_data as pl
 
 
 
-def parent_propagation(filename, tf, cdrag, cdiff):
+def propagate_parent(filename, tf, cdrag, cdiff):
     # propagate parent spacecraft to breakup time
     r, v, coes = vimpel_propagation(filename, tf, cdrag, cdiff)  # angles in rad
 
@@ -52,6 +52,29 @@ def parent_propagation(filename, tf, cdrag, cdiff):
     return parent
 
 
+def propagate_vimpel_objects(filename, tf, cdrag, cdiff, i):
+     
+    r, v, coes = vimpel_propagation(filename, tf, cdrag, cdiff, index=i)  # angles in rad
+
+    # calculate period, perigee, and apogee
+    period = 2 * np.pi / np.sqrt(pl.earth['mu'] / coes[0] ** 3)  # [sec]
+    perigee = coes[0] * (1 - coes[1])  # [km] perigee
+    apogee = coes[0] * (1 + coes[1])   # [km] apogee 
+
+    # calculate radial, downrange, and crossrange velocity components
+    # dv_radial, dv_downrange, dv_crossrange = get_velocity_perturbations(parent, sma, ecc, inc, ta, aop, v,
+    #                                                                     method='Exact')
+     
+    # calculate angular distribution of fragments
+    # dV = np.sqrt(dv_radial**2 + dv_downrange**2 + dv_crossrange**2)
+    # angular_latitude, angular_longitude = get_angular_distribution(dv_radial, dv_downrange, dv_crossrange, dV)  # [deg]
+
+    # return period, perigee, apogee, dv_radial, dv_downrange, dv_crossrange, angular_latitude, angular_longitude
+    return period, perigee, apogee
+
+
+def propagate_perturbed_vimpel_objects():
+
 
 if __name__ == '__main__':
     " ----- INPUT ---------------------------- "
@@ -68,41 +91,36 @@ if __name__ == '__main__':
     file_path = os.path.join(os.environ["FRAG_SRC"], "catalog_files", filename)
     parent_path = os.path.join(os.environ["FRAG_SRC"], "catalog_files", case['Vimpel parent file'])
 
+    # find parent properties at time of breakup
+    parent = propagate_parent(filename, case['breakup time'], cdrag, cdiff)
+
     df_vimpel = pd.read_csv(file_path, header=None)
     num_frags = len(df_vimpel)  # number of objects in TLE
 
-    # find the Cartesian state of each fragment at the given Vimpel epoch
+    # find the ECI state of each fragment at the given Vimpel epoch
     r_vimpel = []
     v_vimpel = []
-    sigma_intrack = []
+    std_intrack = []
     for i in range(num_frags):
         vimpel_data = extract_vimpel(file_path, i)
         r, v = vimpel_to_state(vimpel_data['coes'])
         r_vimpel.append(r)  # [km] fragment position vector
         v_vimpel.append(v)  # [km/s] fragment velocity vector
-        sigma_intrack.append(vimpel_data['std_r'])   # [km] fragment position uncertainty in transverse direction
+        std_intrack.append(vimpel_data['std_r'])   # [km] fragment position uncertainty in transverse direction
 
-    # find parent properties at time of breakup
-    parent = parent_propagation(filename, case['breakup time'], cdrag, cdiff)
+    # obtain position uncertainty standard deviation information for each fragment - convert from ICO to RDX to ECI frame
+    dcm_rdx2eci = xyz_to_rdx(parent['r'], parent['v]']).T
+    dcm_ico2rdx = ico_to_rdx(parent['r'], parent['v]'])
+    dcm_ico2eci = dcm_ico2rdx @ dcm_rdx2eci
+    sig_avg = np.mean(std_intrack)
+    std_r = []
+    for i in range(num_frags):
+        if std_intrack[i] == 0:
+            std_intrack[i] = sig_avg
+        std_ico = [std_intrack[i], std_intrack[i]/5.608, std_intrack[i]/5.663]  # based on Baietto thesis
+        std_r.append(dcm_ico2eci @ std_ico)
 
-    # obtain position uncertainty standard deviation information
+    # propagate fragments with no uncertainty
+        
 
-    "Convert position uncertainty values from ICO to RDX to ECI frame"
-    DCM_ICO2RDX = ICO_to_RDX(r_parent_breakup, v_parent_breakup)
-    DCM_ECI2RDX = parent['rotMatrix']
-    # DCM_ECI2RDX = ECI_to_RDX(raan_parent_breakup*deg2rad, inc_parent_breakup*deg2rad, aop_parent_breakup*deg2rad,
-    #                          ta_parent_breakup*deg2rad)
-    # DCM_RDX2ECI = np.array(DCM_ECI2RDX).T.tolist()
-    DCM_RDX2ECI = DCM_ECI2RDX.T
-    print(DCM_RDX2ECI)
-    sigr_avg = np.mean(sigma_intrack)
-    sigma_r = []
-    for ii in range(num_frags):
-        if sigma_intrack[ii] == 0:
-            sigma_intrack[ii] = sigr_avg
-        sigma_ICO = [sigma_intrack[ii], sigma_intrack[ii]/5.608, sigma_intrack[ii]/5.663]  # based on Baietto thesis
-        sigma_RDX = (np.matmul(DCM_ICO2RDX, sigma_ICO))
-        sigma_r.append(np.matmul(DCM_RDX2ECI, sigma_RDX))
-        # print(ii)
-        # print(sigma_ICO)
-        # print(sigma_RDX)
+    # propagate fragments with uncertainty
